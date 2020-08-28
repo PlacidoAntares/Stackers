5a2039a79546ea0fd39c62471ff2d292027eaa02
placido_penitente <antaresplacido@gmail.com>
2020-08-28 17:54:31 +0800

    first commit

---
+++ Assets/ExposionControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExposionControl : MonoBehaviour {
+
+	public float duration;
+	public bool isAwake;
+	// Use this for initialization
+	void Start () {
+		isAwake = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isAwake == true) {
+			InvokeRepeating ("Disable", duration, 0);
+			isAwake = false;
+		}
+	}
+	void Disable()
+	{
+		isAwake = false;
+		this.gameObject.SetActive (false);
+	}
+}
---
+++ Assets/StackerzPackage/CubeCounter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CubeCounter : MonoBehaviour {
+
+	public int CubesActive;
+	public int CubesDestroyed;
+	public int CubeLimit;
+	public int Score;
+	public bool GameIsOver;
+	public GameObject Player;
+	PlayerControls P_C;
+	public Text ScoreText;
+	public Text CubeDestroyed;
+	public Text Time;
+	public Text Cubes_Present;
+	// Use this for initialization
+	void Start () {
+		P_C = Player.GetComponent<PlayerControls> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (CubesActive >= CubeLimit) 
+		{
+			P_C.GameOver = true;
+			GameIsOver = true;
+		}
+		Score_System ();
+	}
+
+	void Score_System()
+	{
+		if (GameIsOver != true) 
+		{
+			ScoreIncrementing ();
+		}
+	}
+	void ScoreIncrementing()
+	{
+		ScoreText.text = "Current Score:" + Score.ToString ();
+		CubeDestroyed.text = "Cubes Destroyed:" + CubesDestroyed.ToString (); 
+		Cubes_Present.text = "Cubes Present:" + CubesActive.ToString (); 
+
+	}
+}
